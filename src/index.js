e70d860cbba4155405982139198dcade1f13b259
coleus99 <christopherjohncoles@gmail.com>
2020-12-02 21:46:05 +0000

    API setup to add list of product names to shop page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './Router';
+import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
